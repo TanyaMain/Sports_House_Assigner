52163747fe67ed12fed9c56fce73a429958ac005
Tanyaradzwa Mapuwei <Tanyaradzwa Mapuwei@DESKTOP-KD40N9V>
2022-07-26 21:44:59 +0200

    new commit

---
+++ src/sports/house/assigner/SportsHouseAssigner.java
@@ -0,0 +1,35 @@
+
+package sports.house.assigner;
+
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+
+public class SportsHouseAssigner {
+
+    public static void main(String[] args) {
+        
+        String[] sportsHouse = {"BLUE", "WHITE", "RED"};
+        Random rand = new Random();
+        //int colours = rand.nextInt(sportsHouse.length);
+        //int numbers = rand.nextInt(1000);
+        
+        String firstName = JOptionPane.showInputDialog(null,    "Please enter the first student name.");
+        String secondName = JOptionPane.showInputDialog(null,   "Please enter the second student name.");
+        String thirdName = JOptionPane.showInputDialog(null,    "Please enter the third student name.");
+        
+        
+        
+          JOptionPane.showMessageDialog(null,
+                   "School House Assignment\n"
+                  + "*********************************\n"
+                  + firstName + " assigned to the " + sportsHouse[ rand.nextInt(sportsHouse.length)] 
+                  + " house with student number "  + sportsHouse[ rand.nextInt(sportsHouse.length)] + rand.nextInt(1000) + "\n"
+                  + secondName + " assigned to the " + sportsHouse[ rand.nextInt(sportsHouse.length)] 
+                  + " house with student number " + sportsHouse[ rand.nextInt(sportsHouse.length)] + rand.nextInt(1000) + "\n" 
+                  + thirdName + " assigned to the " + sportsHouse[ rand.nextInt(sportsHouse.length)]
+                  + " house with student number "  + sportsHouse[ rand.nextInt(sportsHouse.length)] + rand.nextInt(1000));
+    }
+    
+}
+
